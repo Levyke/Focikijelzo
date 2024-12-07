84392f877606c2fc0bb31d1e0f52c8c206433693
Levyke <114740589+Levyke@users.noreply.github.com>
2024-12-07 15:31:24 +0100

    - adatok html és JavaScript elkezdése (gólok növelése, csökkentése)

---
+++ script.js
@@ -0,0 +1,65 @@
+let adatokdiv = document.getElementById("adatok")
+let jatekadatdiv = document.getElementById("jatekadat")
+function start()
+{
+    let adatokdiv = document.getElementById("adatok")
+    let jatekadatdiv = document.getElementById("jatekadat")
+    let csapat1 = document.getElementById("csapat1").value;
+    let csapat2 = document.getElementById("csapat2").value;
+    let idobeallitas = document.getElementById("idobeallitas").value;
+    adatokdiv.style.display = "none";
+    jatekadatdiv.style.display = "block";
+    if (csapat1 == "" || csapat2 == "" || idobeallitas == "0"){
+        alert("Kérlek, tölts ki minden mezőt!"); 
+        document.getElementById("adatok").style.display = "block";
+        document.getElementById("jatekadat").style.display = "none";
+    }
+}
+
+function ujmeccs()
+{
+    document.getElementById("adatok").style.display = "block";
+    document.getElementById("jatekadat").style.display = "none";
+    csapat1.value = "";
+    csapat2.value = "";
+    idobeallitas.value = 0;
+}
+
+let cs1Pont = 0;
+let cs2Pont = 0;
+function pontok() 
+{
+    document.getElementById('cs1Pont').textContent = cs1Pont;
+    document.getElementById('cs2Pont').textContent = cs2Pont;
+}
+
+function cs1minusz()
+{
+    if(cs1Pont > 0)
+    {
+        cs1Pont--;
+    }
+    
+    pontok()
+}
+function cs1plusz()
+{
+    cs1Pont++;
+    pontok()
+}
+
+function cs2minusz()
+{
+    if (cs2Pont > 0)
+    {
+        cs2Pont--;
+    }
+    pontok()
+}
+
+function cs2plusz()
+{
+    cs2Pont++;
+    pontok()
+}
+
